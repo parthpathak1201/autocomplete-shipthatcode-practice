f1ff635ae2f19c3cf90184cf604c738056f43232
Parth Pathak <65105756+parthpathak1201@users.noreply.github.com>
2026-09-21 11:31:05 +0530

    initial commit

---
+++ shipthatcode-build-trie-autocomplete-cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+//#include <bits/stdc++.h>
+
+
+// TODO (trie-basics): implement per the lesson description.
+
+int main() {
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) continue;
+        std::cout << "TODO" << "\n";
+    }
+}
